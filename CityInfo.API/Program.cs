26aa295ab5e3b76622efb68a665802988981b938
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-11-23 16:11:56 +0100

    Adding a downloadable file to API

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,14 +1,15 @@
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.StaticFiles;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(options => {options.ReturnHttpNotAcceptable = true;})
+builder.Services.AddControllers(options => { options.ReturnHttpNotAcceptable = true; })
     .AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
@@ -25,9 +26,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 app.Run();
