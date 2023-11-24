0b7f45956e1b7c1a427e24523caf207a108cd75d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-11-24 16:09:57 +0100

    Safely Storing Connection String

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -35,7 +35,9 @@ builder.Services.AddTransient<IMailService, CloudMailService>();
 
 builder.Services.AddSingleton<CitiesDataStore>();
 
-builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite("Data Source=CityInfo.db"));
+builder.Services.AddDbContext<CityInfoContext>(
+    dbContextOptions => dbContextOptions.UseSqlite(
+        builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
 var app = builder.Build();
 
